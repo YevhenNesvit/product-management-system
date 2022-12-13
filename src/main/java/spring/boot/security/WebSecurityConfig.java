b183b2e84dec0daad49d45d6bfaaac4a074c0a04
Yevhen <yevhen.nesvit@gmail.com>
2022-12-13 11:02:09 +0200

    Add security functionality

--- src/main/java/spring/boot/security/WebSecurityConfig.java
+++ src/main/java/spring/boot/security/WebSecurityConfig.java
@@ -3,23 +3,46 @@ package spring.boot.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/index").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/products/*").hasRole("ADMIN")
+public class WebSecurityConfig {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/index").hasAnyRole("ADMIN", "USER")
+//                .antMatchers("/products/*").hasRole("ADMIN")
 //                .antMatchers("/user").hasRole("USER")
+//                .and()
+//                .formLogin()
+//                .and()
+//                .httpBasic();
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/products/**").hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/index", false)
+                .failureUrl("/login.html?error=true")
                 .and()
-                .httpBasic();
+                .logout()
+                .logoutUrl("/logout");
+
+        return http.build();
+
     }
 
     @Bean
