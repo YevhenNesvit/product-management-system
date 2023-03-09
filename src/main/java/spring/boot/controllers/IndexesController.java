219f512c828a46ceb72db2d446416d9671dbfde5
Yevhen <yevhen.nesvit@gmail.com>
2023-03-09 17:48:14 +0200

    Add logged-in user first name and last name visible on all pages

--- src/main/java/spring/boot/controllers/IndexesController.java
+++ src/main/java/spring/boot/controllers/IndexesController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +21,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @AllArgsConstructor
-@Controller
+@ControllerAdvice
 @RequestMapping("")
 public class IndexesController {
     @Autowired
