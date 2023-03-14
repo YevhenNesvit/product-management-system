dca50235aec67025a6f026ceb118fdd05f037e7a
Yevhen <yevhen.nesvit@gmail.com>
2023-03-14 10:57:49 +0200

    Change validation logic of create user functionality

--- src/main/java/spring/boot/controllers/UserController.java
+++ src/main/java/spring/boot/controllers/UserController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import spring.boot.converter.RoleConverter;
@@ -13,6 +15,7 @@ import spring.boot.services.RoleService;
 import spring.boot.services.UserService;
 
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @Controller
@@ -27,39 +30,42 @@ public class UserController {
     @Autowired
     private final RoleConverter converter;
 
-    @RolesAllowed("ADMIN")
     @GetMapping("/createUserForm")
     public ModelAndView createUserForm() {
-        ModelAndView mav = new ModelAndView("users/createUserForm");
-        mav.addObject("users", userService.getUsers());
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("UserDto", new UserDto());
 
         return mav;
     }
 
-    @PostMapping("/userCreated")
-    public ModelAndView createUser(@ModelAttribute("email") String email, @ModelAttribute("password") String password,
-                                   @ModelAttribute("firstName") String firstName, @ModelAttribute("lastName") String lastName,
-                                   @ModelAttribute("roleName") String roleName, UserDto user, RoleDao role) {
+    @PostMapping("/createUserForm")
+    public ModelAndView createUser(@ModelAttribute("UserDto") @Valid UserDto user, BindingResult bindingResult,
+                                   @ModelAttribute("roleName") String roleName, Model model, RoleDao role) {
+        if (bindingResult.hasErrors()) {
 
-        if (userService.IsUserEmailExists(email)) {
+            return new ModelAndView("users/createUserForm");
+        } else if (userService.IsUserEmailExists(user.getEmail())) {
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("userExists", "User already exists!");
 
-            return new ModelAndView("users/userAlreadyExists");
+            return mav;
         } else if (roleService.IsRoleNameExists(roleName)) {
+            model.addAttribute("UserDto", user);
+
             role.setId(roleService.getRoleIdByName(roleName));
             role.setName(roleName);
 
-            user.setEmail(email);
-            user.setPassword(passwordEncoder.encode(password));
-            user.setFirstName(firstName);
-            user.setLastName(lastName);
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setRole(converter.from(role));
 
             userService.create(user);
 
             return new ModelAndView("users/userCreated");
         } else {
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("roleNotExists", "Role does not exist!");
 
-            return new ModelAndView("roles/roleNameNotExists");
+            return mav;
         }
     }
 
